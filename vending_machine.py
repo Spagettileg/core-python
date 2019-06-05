ee40a7ede27f98965239d75efa1797641d081a03
Spagettileg <paul.b.friel@me.com>
2019-06-05 14:29:35 +0000

    Initial commit

---
+++ vending_machine.py
@@ -0,0 +1,10 @@
+from byotest import * 
+
+# Function 1
+def get_change(amount): 
+    return []
+# Test 1 = When the amount of chnage we require is zero, then no coins are returned back
+test_are_equal(get_change(0),[])
+
+print("All tests pass!")
+
