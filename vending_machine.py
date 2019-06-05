8e83b30d62606a471a04bbd6124a93674e686d02
Spagettileg <paul.b.friel@me.com>
2019-06-05 20:31:02 +0000

    Added 5 tests to generic code for different coin change scenarios

--- vending_machine.py
+++ vending_machine.py
@@ -2,9 +2,39 @@ from byotest import *
 
 # Function 1
 def get_change(amount): 
-    return []
+    if amount == 0:
+        return []
+        
+    if amount in [100, 50, 20, 10, 5, 2, 1]:
+        return [amount]
+        
+    change = []
+    for coin in [100, 50, 20, 10, 5, 2, 1]:
+        if coin <= amount:   # Change is always going to be less, or in rare occasions, equal to amount spent
+            amount -= coin   # Required to ensure the code deducts the amount of the coin from what was sent into the vending machine  
+            change.append(coin)
+        
+        return change
 # Test 1 = When the amount of chnage we require is zero, then no coins are returned back
 test_are_equal(get_change(0),[])
+
+# Test 2 = The money required is represented by a single coin. Test is passed by creating if statement for Test 1, then return [1] for Test 2.
+test_are_equal(get_change(1),[1])
+
+# Test 3 = To send in a different coin to the vending machine. Test is passed by changing 'return [1] to return [amount]'. 
+# The Tests are now repeated to cover all coin values.
+test_are_equal(get_change(2),[2]) 
+test_are_equal(get_change(5),[5]) 
+test_are_equal(get_change(10),[10]) 
+test_are_equal(get_change(20),[20]) 
+test_are_equal(get_change(50),[50]) 
+test_are_equal(get_change(100),[100]) 
+
+# Test 4 = Create a test where we need change in more than 1 coin. Below = 3p change, in 2p + 1p. 
+test_are_equal(get_change(3),[2,1])
+
+# Test 5 = Similar to Test 4. However, need to create a generic code and ensure correct change is given from valuie sent into machine.
+test_are_equal(get_change(7),[5,2])
 
 print("All tests pass!")
 
